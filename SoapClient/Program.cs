d3511295e403a836e5d53bc195bef8c15f080504
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-01-18 10:30:46 +0100

    Add project files.

---
+++ SoapClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using ServiceReference1;
+using System;
+
+namespace SoapClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new CurrencyServiceClient();
+
+            Amount val = new();
+            val.Value = 15;
+            val.Currency = "USD";
+
+            Amount result = client.ConvertToDkkAsync(val).Result.Body.ConvertToDkkResult;
+            Console.WriteLine($"{result.Value} {result.Currency}");
+        }
+    }
+}
